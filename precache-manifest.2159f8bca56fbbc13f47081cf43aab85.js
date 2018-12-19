3fc3bb8b85ae0395f42f3227146073f6b3f4f2e9
yugeon <yugeon@kdn.ktguide.com>
2018-12-19 14:08:32 +0900

    Updates

---
+++ precache-manifest.2159f8bca56fbbc13f47081cf43aab85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "0695a3b8a26de542dd48",
+    "url": "/movie_app/static/js/main.0695a3b8.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/movie_app/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "0695a3b8a26de542dd48",
+    "url": "/movie_app/static/css/main.06638153.chunk.css"
+  },
+  {
+    "revision": "d38b7429c2a70bce65e739a566ae647e",
+    "url": "/movie_app/index.html"
+  }
+];
